5b246b45c479e5387c4aeb37a04a0a8a74ce2460
Sujith Kumar Appikatla <sujith.appikatla@gmail.com>
2024-08-05 00:46:15 +0530

    Add files via upload

--- sketch_cnn.py
+++ sketch_cnn.py
@@ -5,7 +5,7 @@
 # ### Package Install
 
 # %%
-# ! pip install tensorflow-gpu numpy matplotlib scikit-learn
+#! pip install tensorflow-gpu numpy matplotlib scikit-learn
 
 # %%
 import tensorflow as tf
@@ -60,7 +60,48 @@ CATEGORIES = [
     "calendar", "camel", "camera", "camouflage", "campfire", "candle", "cannon", 
     "canoe", "car", "carrot", "castle", "cat", "ceiling fan", "cello", 
     "cell phone", "chair", "chandelier", "church", "circle", "clarinet", 
-    "clock", "cloud", "coffee cup", "compass", "computer", "cookie", "cooler"
+    "clock", "cloud", "coffee cup", "compass", "computer", "cookie", "cooler", 
+    "couch", "cow", "crab", "crayon", "crocodile", "crown", "cruise ship", 
+    "cup", "diamond", "dishwasher", "diving board", "dog", "dolphin", "donut", 
+    "door", "dragon", "dresser", "drill", "drums", "duck", "dumbbell", "ear",
+    "elbow", "elephant", "envelope", "eraser", "eye", "eyeglasses", "face", 
+    "fan", "feather", "fence", "finger", "fire hydrant", "fireplace", 
+    "firetruck", "fish", "flamingo", "flashlight", "flip flops", "floor lamp", 
+    "flower", "flying saucer", "foot", "fork", "frog", "frying pan", "garden", 
+    "garden hose", "giraffe", "goatee", "golf club", "grapes", "grass", 
+    "guitar", "hamburger", "hammer", "hand", "harp", "hat", "headphones", 
+    "hedgehog", "helicopter", "helmet", "hexagon", "hockey puck", "hockey stick", 
+    "horse", "hospital", "hot air balloon", "hot dog", "hot tub", "hourglass", 
+    "house", "house plant", "hurricane", "ice cream", "jacket", "jail", 
+    "kangaroo", "key", "keyboard", "knee", "knife", "ladder", "lantern", 
+    "laptop", "leaf", "leg", "light bulb", "lighter", "lighthouse", "lightning", 
+    "line", "lion", "lipstick", "lobster", "lollipop", "mailbox", "map", 
+    "marker", "matches", "megaphone", "mermaid", "microphone", "microwave", 
+    "monkey", "moon", "mosquito", "motorbike", "mountain", "mouse", "moustache", 
+    "mouth", "mug", "mushroom", "nail", "necklace", "nose", "ocean", "octagon", 
+    "octopus", "onion", "oven", "owl", "paintbrush", "paint can", "palm tree", 
+    "panda", "pants", "paper clip", "parachute", "parrot", "passport", "peanut", 
+    "pear", "peas", "pencil", "penguin", "piano", "pickup truck", "picture frame", 
+    "pig", "pillow", "pineapple", "pizza", "pliers", "police car", "pond", 
+    "pool", "popsicle", "postcard", "potato", "power outlet", "purse", "rabbit", 
+    "raccoon", "radio", "rain", "rainbow", "rake", "remote control", "rhinoceros", 
+    "rifle", "river", "roller coaster", "rollerskates", "sailboat", "sandwich", 
+    "saw", "saxophone", "school bus", "scissors", "scorpion", "screwdriver", 
+    "sea turtle", "see saw", "shark", "sheep", "shoe", "shorts", "shovel", 
+    "sink", "skateboard", "skull", "skyscraper", "sleeping bag", "smiley face", 
+    "snail", "snake", "snorkel", "snowflake", "snowman", "soccer ball", "sock", 
+    "speedboat", "spider", "spoon", "spreadsheet", "square", "squiggle", 
+    "squirrel", "stairs", "star", "steak", "stereo", "stethoscope", "stitches", 
+    "stop sign", "stove", "strawberry", "streetlight", "string bean", "submarine", 
+    "suitcase", "sun", "swan", "sweater", "swing set", "sword", "syringe", 
+    "table", "teapot", "teddy-bear", "telephone", "television", "tennis racquet", 
+    "tent", "The Eiffel Tower", "The Great Wall of China", "The Mona Lisa", 
+    "tiger", "toaster", "toe", "toilet", "tooth", "toothbrush", "toothpaste", 
+    "tornado", "tractor", "traffic light", "train", "tree", "triangle", 
+    "trombone", "truck", "trumpet", "t-shirt", "umbrella", "underwear", "van", 
+    "vase", "violin", "washing machine", "watermelon", "waterslide", "whale", 
+    "wheel", "windmill", "wine bottle", "wine glass", "wristwatch", "yoga", 
+    "zebra", "zigzag"
 ]
 
 DATA_DIR = 'quickdraw_data'
@@ -152,31 +193,13 @@ class QuickDrawDataGenerator(Sequence):
 def create_model(num_classes):
     """Create the CNN model."""
     model = models.Sequential([
-        # First convolutional block
         layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
-        layers.BatchNormalization(),
         layers.MaxPooling2D((2, 2)),
-        layers.Dropout(0.25),
-        
-        # Second convolutional block
         layers.Conv2D(64, (3, 3), activation='relu'),
-        layers.BatchNormalization(),
         layers.MaxPooling2D((2, 2)),
-        layers.Dropout(0.25),
-
-        # Third convolutional block
-        layers.Conv2D(128, (3, 3), activation='relu'),
-        layers.BatchNormalization(),
-        layers.MaxPooling2D((2, 2)),
-        layers.Dropout(0.25),
-
-        # Flatten and fully connected layers
+        layers.Conv2D(64, (3, 3), activation='relu'),
         layers.Flatten(),
-        layers.Dense(128, activation='relu'),
-        layers.BatchNormalization(),
-        layers.Dropout(0.5),
-        
-        # Output layer
+        layers.Dense(64, activation='relu'),
         layers.Dense(num_classes, activation='softmax')
     ])
     return model
@@ -207,7 +230,7 @@ def train_model(model, train_generator, val_generator, epochs):
     # optimizer = mixed_precision.LossScaleOptimizer(tf.keras.optimizers.Adam())
     
     model.compile(optimizer="adam",
-                  loss='sparse_categorical_crossentropy',
+                  loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
     model.summary()
